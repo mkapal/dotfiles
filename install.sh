1d434e9379051b93693e28a9a8fc98cab1aaa8c1
Martin Kapal <flamecze@gmail.com>
2018-07-22 21:42:47 +0200

    Automatically install tmux plugins

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Tmux Plugin Manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 rm ~/.aliases
 ln -s ~/configs/.aliases ~/
 
+# Tmux configuration
 rm ~/.tmux.conf
 ln -s ~/configs/.tmux.conf ~/
+
+# Install all tmux plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 rm ~/.zshrc
 ln -s ~/configs/.zshrc ~/
