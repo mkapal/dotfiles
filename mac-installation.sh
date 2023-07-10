99d7589a233564ac7339e010047d8a79ee0fc84c
Martin Kapal <makap@ciklum.com>
2023-07-10 19:29:06 +0200

    Update Brewfile, zed config, add mac scripts

---
+++ mac-installation.sh
@@ -0,0 +1,5 @@
+xcode-select --install
+sudo gem install homesick
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+homesick clone mkapal/dotfiles
+homesick link dotfiles
